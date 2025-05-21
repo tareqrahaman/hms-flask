57561d4fad346edaf414c984f8db42369d907f23
tareqrahaman <tareqrahaman565@gmail.com>
2025-05-21 17:39:19 +0600

    logo added & header modified

--- app.py
+++ app.py
@@ -1,33 +1,55 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from routes.patient_routes import patient_bp
 from routes.admission_routes import admission_bp
-from jinja2 import FileSystemLoader
 from routes.doctor_routes import doctor_bp
 from routes.appointment_routes import appointment_bp
+from db import SessionLocal
+from models.patient import Patient
+from models.doctor import Doctor
+from models.admission import Admission
+from models.appointment import Appointment
 
+from jinja2 import FileSystemLoader
 
 app = Flask(__name__)
-#below is force to block caching
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.debug = True
 app.jinja_loader = FileSystemLoader('templates')
 
-app.secret_key = 'Monia47'  # Needed for session management
+app.secret_key = 'Monia47'
 
 app.register_blueprint(patient_bp, url_prefix='/patients')
 app.register_blueprint(admission_bp, url_prefix='/admissions')
 app.register_blueprint(doctor_bp, url_prefix='/doctors')
 app.register_blueprint(appointment_bp, url_prefix='/appointments')
 
-# Hardcoded credentials (for now)
 USERS = {
     'admin': '123'
 }
 
 @app.route('/')
 def home():
+    session_db = SessionLocal()
+    num_patients = session_db.query(Patient).count()
+    num_doctors = session_db.query(Doctor).count()
+    num_admissions = session_db.query(Admission).count()
+    num_appointments = session_db.query(Appointment).count()
+
+    inpatients = session_db.query(Patient).filter_by(IsInpatient=True).count()
+    outpatients = num_patients - inpatients
+    session_db.close()
+
     is_logged_in = 'user' in session
-    return render_template('index.html', is_logged_in=is_logged_in)
+    return render_template(
+        'index.html',
+        is_logged_in=is_logged_in,
+        num_patients=num_patients,
+        num_doctors=num_doctors,
+        num_admissions=num_admissions,
+        num_appointments=num_appointments,
+        inpatients=inpatients,
+        outpatients=outpatients
+    )
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
